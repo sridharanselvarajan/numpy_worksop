d742b9d6c42bf01f83ffafdc02d6831695b247ce
sridharanselvarajan <152093454+sridharanselvarajan@users.noreply.github.com>
2024-05-22 11:46:09 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,8 @@
 # find if the given number is a palindrome or not?
+original_string="Malayalam";
+reversed_string=original_string[::-1]
+if(original_string==reversed_string):
+   print("It is palindrome")
+else:
+    print("It is not a palindrome")
+    
