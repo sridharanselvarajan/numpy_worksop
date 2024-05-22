2295850ce00ef068551bd9be1315aaf19659f9c8
sridharanselvarajan <152093454+sridharanselvarajan@users.noreply.github.com>
2024-05-22 11:47:24 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,11 @@
 #write a program to find the factorial of a nummber
+
+def factorial_recursive(n):
+    if n < 0:
+        print("factorial of a number is not possible")
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial_recursive(n - 1)
+
+n=int(input("Enter the number"))
+print(factorial_recursive(n)) 
