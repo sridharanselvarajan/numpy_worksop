e391d2fabca96179f2ac0ae85bcb51b2a5335262
sridharanselvarajan <152093454+sridharanselvarajan@users.noreply.github.com>
2024-05-22 11:47:58 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,12 @@
 #write a program to find the sum of digits of a given number'
+def sum_of_digits(n):
+    sum = 0
+    n = abs(n) 
+    while n > 0:
+        r = n % 10
+        sum += r
+        n //= 10  
+    print(sum)
+
+num = int(input("Enter the number: "))
+sum_of_digits(num)
