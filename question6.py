cf9f1b62c026beb22f07c8db5d716796f64b0b67
sridharanselvarajan <152093454+sridharanselvarajan@users.noreply.github.com>
2024-05-22 11:49:17 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,10 @@
 #write a program to find the maximum of two numbers
+def find_maximum(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+maximum = find_maximum(num1, num2)
+print("The maximum of the two numbers is:", maximum)
